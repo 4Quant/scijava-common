fa99bbce98012489104bf64cf8e338fd98b5626e
Curtis Rueden <ctrueden@wisc.edu>
2014-06-07 11:37:55 +0200

    Add a location service for managing data locations
    A data "location" is a path (source and/or destination) that points
    to data. Many data locations, such as files on disk and remote
    URLs, can be represented as strings, so the LocationService knows
    how to wrap a string as a data location of the appropriate type.

---
+++ src/main/java/org/scijava/io/Location.java
@@ -0,0 +1,57 @@
+/*
+ * #%L
+ * SciJava Common shared library for SciJava software.
+ * %%
+ * Copyright (C) 2009 - 2014 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.io;
+
+import org.scijava.plugin.WrapperPlugin;
+
+/**
+ * A data descriptor, such as a file on disk, a remote URL, or a database
+ * connection.
+ * 
+ * @author Curtis Rueden
+ */
+public interface Location extends WrapperPlugin<String> {
+
+	/**
+	 * Gets a string which unambiguously describes the data location. Passing this
+	 * path to {@link LocationService#create(String)} will return a
+	 * {@link Location} instance equal to this one.
+	 * <p>
+	 * Note that some types of locations may include sensitive information (e.g.,
+	 * a database connection may include a session key or user login credentials),
+	 * so it is advised to use the {@link #toString()} method instead of
+	 * {@link #getPath()} when printing the location.
+	 * </p>
+	 */
+	String getPath();
+
+}
